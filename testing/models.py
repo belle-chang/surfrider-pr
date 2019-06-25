c06cb46901efe3666aff60576e289d8fe07421a9
Anabelle <bellech88@gmail.com>
2019-06-25 10:00:13 -0400

    hehe

--- testing/models.py
+++ testing/models.py
@@ -5,6 +5,8 @@ class Beach(models.Model):
     beach_name = models.CharField(max_length=200)
     date_visited = models.DateTimeField('date visited')
     # count = models.IntegerField(default=0)
+    def __str__(self):
+        return '%s' % (self.beach_name)
 
 class BeachNameID(models.Model):
     beach_name = models.CharField(max_length=200)
--- testing/views.py
+++ testing/views.py
@@ -4,11 +4,16 @@ import datetime
 # Create your views here.
 def reroute_to_surfrider(request, beach_id):
     url = "https://www.surfrider.org/blue-water-task-force/beach/" + str(beach_id)
+    print(str(beach_id))
     beach_obj = BeachNameID.objects.get(beach_id=beach_id)
     beach_name = beach_obj.beach_name
+    print("beach obj" + beach_obj.beach_name)
+    print("beach name" + beach_name)
+    date_and_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+    # print("date and time" + date_and_time)
 
     beach_info = Beach(beach_name=beach_name,
-                       date_visited=datetime.datetime.now()
+                       date_visited=date_and_time,
                        )
     beach_info.save()
     return redirect(url)
