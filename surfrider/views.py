542e62a89c7c674226da9da5207c7c75e892d457
Anabelle <bellech88@gmail.com>
2019-06-25 11:19:46 -0400

    yoyo

--- surfrider/views.py
+++ surfrider/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, render_to_response
-# from ..testing.models import BeachNameID, Beach
+from testing.models import BeachNameID, Beach
 import datetime
 
 
@@ -8,13 +8,13 @@ def index(request):
     # if request.user.is_authenticated:
     #     return render(request, 'home.html')
     # # return HttpResponse("welcome.html")
-    # beach_dictionary = {}
-    # all_beaches = Beach.objects.all()
-    # for beach in all_beaches:
-    #     if BeachNameID.objects.filter(beach_name=beach.beach_name).exists():
-    #         beach_dictionary[beach.beach_name] = BeachNameID.objects.filter(beach_name=beach.beach_name).count()
+    beach_dictionary = {}
+    all_beaches = Beach.objects.all()
+    for beach in all_beaches:
+        if BeachNameID.objects.filter(beach_name=beach.beach_name).exists():
+            beach_dictionary[beach.beach_name] = BeachNameID.objects.filter(beach_name=beach.beach_name).count()
     # {'beach_dict' : beach_dictionary}
-    return render(request, 'welcome.html' )
+    return render(request, 'welcome.html', {'beach_dict' : beach_dictionary})
 
 # def reroute_to_surfrider(request, beach_id):
 #     url = "https://www.surfrider.org/blue-water-task-force/beach/" + beach_id
