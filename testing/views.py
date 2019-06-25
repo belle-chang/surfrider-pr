3dfee771a86f5865f211ea430225f0ff51c06778
Anabelle <bellech88@gmail.com>
2019-06-25 10:45:11 -0400

    hehe

--- testing/views.py
+++ testing/views.py
@@ -6,12 +6,11 @@ import datetime
 def reroute_to_surfrider(request, beach_id):
     if beach_id == 666:
         return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    url = "https://www.surfrider.org/blue-water-task-force/beach/" + str(beach_id)
+    url = "https://www.surfrider.org/blue-water-task-force/beach/" + str(beach_id[:3])
     print(str(beach_id))
     beach_obj = BeachNameID.objects.get(beach_id=beach_id)
     beach_name = beach_obj.beach_name
-    print("beach obj" + beach_obj.beach_name)
-    print("beach name" + beach_name)
+    print("beach name " + beach_name)
     date_and_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
     # print("date and time" + date_and_time)
 
